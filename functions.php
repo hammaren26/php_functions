2b6b364d62ab651e54aed40646de9d42d48dff7b
Vyacheslav <hammaren23@gmail.com>
2023-02-06 08:42:54 +0600

    'sdf'

---
+++ functions.php
@@ -0,0 +1,46 @@
+<?php
+
+
+/*
+ * Возвращает код видео из ссылки на youtube
+ * $url - УРЛ видео youtube
+ * */
+if (!function_exists('GetYouTubeId')) {
+    function GetYouTubeId($url)
+    {
+        $videoId = '';
+        //This is a general function for generating an embed link of an FB/Vimeo/Youtube Video.
+        $finalUrl = '';
+        if (strpos($url, 'facebook.com/') !== false) {
+            //it is FB video
+        } elseif (strpos($url, 'vimeo.com/') !== false) {
+            //it is Vimeo video
+            $videoId = explode("vimeo.com/", $url)[1];
+            if (strpos($videoId, '&') !== false)
+                $videoId = explode("&", $videoId)[0];
+        } elseif (strpos($url, 'youtube.com/') !== false) {
+            //it is Youtube video
+            $videoId = explode("v=", $url)[1];
+            if (strpos($videoId, '&') !== false)
+                $videoId = explode("&", $videoId)[0];
+        } elseif (strpos($url, 'youtu.be/') !== false) {
+            //it is Youtube video
+            $videoId = explode("youtu.be/", $url)[1];
+            if (strpos($videoId, '&') !== false)
+                $videoId = explode("&", $videoId)[0];
+        }
+        return $videoId;
+    }
+}
+
+
+/*
+ * Проверка на кол-во символов в телефоне
+ */
+if (!function_exists('IsCorrectMobilePhone')) {
+    function IsCorrectMobilePhone($number)
+    {
+        $clearPhone = preg_replace("/[^0-9]/", '', $number);
+        return strlen($clearPhone) === 11;
+    }
+}
